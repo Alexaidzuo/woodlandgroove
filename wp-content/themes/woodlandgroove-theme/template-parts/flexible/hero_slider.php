94a4fe80fd553c788aec7398551275feb4e01781
Aleksandar Ciric <ciricx@gmail.com>
2022-04-15 23:36:52 +0200

    Starting project

---
+++ wp-content/themes/woodlandgroove-theme/template-parts/flexible/hero_slider.php
@@ -0,0 +1,17 @@
+<?php
+
+$title =  get_sub_field('title');
+$subtitle =  get_sub_field('subtitle');
+$background = get_sub_field('image');
+$buttons = get_sub_field('buttons');
+$active = get_sub_field('active');
+
+?>
+
+<section class="hero-slider">
+    <div class="container">
+        <div class="row">
+            <h1>test</h1>
+        </div>
+    </div>
+</section>
