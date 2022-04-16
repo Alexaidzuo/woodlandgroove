7edb2e31cdf5e986e6aeb5a09fcf4b6433275743
Aleksandar Ciric <ciricx@gmail.com>
2022-04-16 12:23:40 +0200

    ITAB section

--- wp-content/themes/woodlandgroove-theme/template-parts/flexible/cta_itab.php
+++ wp-content/themes/woodlandgroove-theme/template-parts/flexible/cta_itab.php
@@ -8,32 +8,48 @@ $active = get_sub_field('active');
 <?php
 if ($active == 1) : ?>
 
-    <section class="itab">
-        <div class="container">
-            <div class="row justify-content-center">
+<section class="itab">
+    <div class="container">
+        <div class="row justify-content-center">
 
-                <?php
-                while (have_rows('itab')) : the_row(); ?>
+        <?php
+        while (have_rows('itab')) : the_row(); ?>
+            <?php
+
+            $image = get_sub_field('image');
+            $title = get_sub_field('title');
+            $subtitle = get_sub_field('subtitle');
+            $button = get_sub_field('button');
+
+            ?>
+
+            <div class="itab__item-inner col-lg-4 col-md-6">
+                <div class="itab__item">
+                    <?php if ($image) { ?>
+                        <div class="itab__item-image">
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                        </div>
+                    <?php } ?>
+
                     <?php
+                    if($title || $subtitle) : ?>
 
-                    $image = get_sub_field('image');
-                    $title = get_sub_field('title');
-                    $subtitle = get_sub_field('subtitle');
-                    $button = get_sub_field('button');
-
-                    ?>
-
-                    <div class="itab__item-inner col-lg-r">
-                        <div class="itab__item">
-                            <?php if ($image) { ?>
-                                <div class="itab__item-image">
-                                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-                                </div>
-                            <?php } ?>
+                        <div class="itab__item-head">
 
                             <?php
                             if ($title) : ?>
-                                <h3 class="itab__item-title"><?php echo $title; ?></h3>
+
+                                <?php
+                                if($button) : ?>
+
+                                <a href="<?php echo $button['url']; ?>"><h3 class="itab__item-title"><?php echo $title; ?></h3></a>
+
+                                <?php else : ?>
+
+                                    <h3 class="itab__item-title"><?php echo $title; ?></h3>
+
+                                <?php endif; ?>
+
                             <?php endif; ?>
 
                             <?php
@@ -41,16 +57,21 @@ if ($active == 1) : ?>
                                 <p class="itab__item-subtitle"><?php echo $subtitle; ?></p>
                             <?php endif; ?>
 
-                            <?php if ($button) : ?>
-                                <a href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>"><?php echo $button['title']; ?></a>
-                            <?php endif; ?>
                         </div>
-                    </div>
+                    <?php endif; ?>
 
-                <?php endwhile; ?>
-
+                    <?php if ($button) : ?>
+                        <div class="itab__item-button">
+                            <a class="btn btn--light-brown" href="<?php echo $button['url']; ?>" target="<?php echo $button['target']; ?>"><?php echo $button['title']; ?></a>
+                        </div>
+                    <?php endif; ?>
+                </div>
             </div>
+
+        <?php endwhile; ?>
+
         </div>
-    </section>
+    </div>
+</section>
 
 <?php endif; ?>
