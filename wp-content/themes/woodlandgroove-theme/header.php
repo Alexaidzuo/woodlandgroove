b6ef84f421222daba7174a6c454fa022883909c4
Aleksandar Ciric <ciricx@gmail.com>
2022-07-08 17:31:18 +0200

    Wall of love and donation

--- wp-content/themes/woodlandgroove-theme/header.php
+++ wp-content/themes/woodlandgroove-theme/header.php
@@ -74,6 +74,7 @@
 					<img class="right-logo" src="<?php echo get_template_directory_uri().'/assets/images/grow.webp'; ?>" alt="Help us Grow">
 
 					<a class="btn btn--cta-donate" href="<?php echo get_site_url()."/donate" ?>">Donate</a>
+					<a class="nav-wall" href="<?php echo get_site_url()."/wall-of-love" ?>">Wall of Love</a>
 				</div>
 
 			</div> <!-- /.row justify-content-between -->
---
+++ wp-content/themes/woodlandgroove-theme/template-parts/flexible/wall_of_love.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * The template part for displaying a Wall of Love content.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package woodlandgroove
+ */
+
+?>
+
+<?php
+
+$active = get_sub_field('active');
+$content = get_sub_field('content');
+$button = get_sub_field('button');
+$wall_of_love = get_sub_field('wall_of_love');
+
+?>
+
+<section class="wall">
+    <div class="container">
+        <div class="row">
+
+        <?php if($content) : ?>
+            <div class="wall__content col-lg-12">
+                <?php echo $content; ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if($button) : ?>
+            <div class="wall__button col-lg-12">
+                <a class="btn btn--cta-donate" href="<?php echo $button['url']; ?>"><?php echo $button['title']; ?></a>
+            </div>
+        <?php endif;?>
+
+        <?php if($wall_of_love) : ?>
+            <?php foreach($wall_of_love as $wall) : ?>
+
+                <?php
+                    $message = $wall['message'];
+                    $name = $wall['name'];
+                    $date = $wall['date'];
+                ?>
+
+                <div class="wall__wrap col-lg-4 col-md-6 col-sm-12">
+                    <div class="wall__inner">
+                        <p class="wall__message"><?php echo $message; ?></p>
+                        <p class="wall__name"><?php echo $name; ?></p>
+                        <p class="wall__date"><?php echo $date; ?></p>
+                    </div>
+
+                </div>
+
+            <?php endforeach; ?>
+        <?php endif; ?>
+
+        </div>
+    </div>
+</section>
